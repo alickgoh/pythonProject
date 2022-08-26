57a4b9e2faf9d25a69128ddb1ec3fc2d6160bab0
alickgoh <gk_lek@hotmail.com>
2022-08-26 20:44:28 +0800

    Update add_two_numbers.py

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,20 +1,22 @@
 class Solution:
-    # def __init__(self, list1, list2):
-    #     self.list1 = list1
-    #     self.list2 = list2
+    def __init__(self, list1, list2):
+        self.list1 = list1
+        self.list2 = list2
 
-    def addTwoNumbers(list1, list2):
-        list1a = "".join(map(str, list1))
-        list2a = "".join(map(str, list2))
+    def addTwoNumbers(self):
+        list1a = "".join(map(str, self.list1))
+        list2a = "".join(map(str, self.list2))
 
         rvs1 = list1a[::-1]
         rvs2 = list2a[::-1]
 
         total = int(rvs1) + int(rvs2)
-        print(total)
+
+        total = list(map(int, str(total)))
+        return total[::-1]
 
 
-l1 = [2, 4, 3]
-l2 = [5, 6, 4]
-
-print(Solution.addTwoNumbers(l1, l2))
+l1 = [0,0]
+l2 = [0]
+Sol = Solution(l1, l2)
+print(Sol.addTwoNumbers())
